718978733105c51532c051a4327b032d10a3eb4c
iuriimattos2 <74837498+iuriimattos2@users.noreply.github.com>
2022-04-10 16:01:12 -0300

    Update Microsoft.PowerShell_profile.ps1

--- ps1/ohmyposh.dev/Microsoft.PowerShell_profile.ps1
+++ ps1/ohmyposh.dev/Microsoft.PowerShell_profile.ps1
@@ -1,42 +1,44 @@
+# PSVersion                      5.1.18362.145
+# PSEdition                      Desktop
+# PSCompatibleVersions           {1.0, 2.0, 3.0, 4.0...}
+# BuildVersion                   10.0.18362.145
+# CLRVersion                     4.0.30319.42000
+# WSManStackVersion              3.0
+# PSRemotingProtocolVersion      2.3
+#SerializationVersion           1.1.0.1
 Import-Module posh-git
 Import-Module oh-my-posh
 Set-PoshPrompt -Theme  ~/mypwsh10k.omp.json
-
 function Get-GitTree { & git log --graph --oneline --decorate $args }
 New-Alias -Name gt -Value Get-GitTree -Force -Option AllScope
-
 function Get-GitStatus { & git status -sb $args }
 New-Alias -Name gs -Value Get-GitStatus -Force -Option AllScope
-
 function Get-GitCommit { & git commit -ev $args }
 New-Alias -Name gc -Value Get-GitCommit -Force -Option AllScope
-
+function Get-GitCommitWip { & git commit -m wip }
+New-Alias -Name gwip -Value Get-GitCommit -Force -Option AllScope
 function Get-GitFetch { & git fetch $args }
 New-Alias -Name gf -Value Get-GitFetch -Force -Option AllScope
-
 function Get-GitBranch { & git branch $args }
 New-Alias -Name gb -Value Get-GitBranch -Force -Option AllScope
-
 function Get-GitRemote { & git remote -v $args }
 New-Alias -Name gr -Value Get-GitRemote -Force -Option AllScope
-
 function Get-GitAdd { & git add --all $args }
 New-Alias -Name ga -Value Get-GitAdd -Force -Option AllScope
-
 function Get-GitPush { & git push $args }
 New-Alias -Name gps -Value Get-GitPush -Force -Option AllScope
-
 function Get-GitPull { & git pull $args }
 New-Alias -Name gpl -Value Get-GitPull -Force -Option AllScope
-
 function Get-GitCheckout { & git checkout $args }
 New-Alias -Name gco -Value Get-GitCheckout -Force -Option AllScope
-
+function Get-GitResetHard & git reset --hard $args }
+New-Alias -Name grh -Value Get-GitResetHard -Force -Option AllScope
+function Get-GitResetHard & git reset --soft $args }
+New-Alias -Name grs -Value Get-GitResetHard -Force -Option AllScope
 function Run-CleanInstall { & mvn clean install -DskipTests }
 New-Alias -Name mvnci -Value Run-CleanInstall -Force -Option AllScope
-
-function Get-GitAdd { & git reset --hard origin/release_branch $args }
-New-Alias -Name grhorb -Value Get-GitAdd -Force -Option AllScope
-
-function Get-GitAdd { & git reset --hard origin/release $args }
-New-Alias -Name grhor -Value Get-GitAdd -Force -Option AllScope
+# Chocolatey profile
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
