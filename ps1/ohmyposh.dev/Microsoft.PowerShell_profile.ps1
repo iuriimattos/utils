2175fa9c757520bb8ae2920ac60fcd52d8a98164
iuriimattos2 <74837498+iuriimattos2@users.noreply.github.com>
2022-04-10 15:52:50 -0300

    Update Microsoft.PowerShell_profile.ps1

--- ps1/ohmyposh.dev/Microsoft.PowerShell_profile.ps1
+++ ps1/ohmyposh.dev/Microsoft.PowerShell_profile.ps1
@@ -3,22 +3,22 @@ Import-Module oh-my-posh
 Set-PoshPrompt -Theme  ~/mypwsh10k.omp.json
 
 function Get-GitTree { & git log --graph --oneline --decorate $args }
-New-Alias -Name t -Value Get-GitTree -Force -Option AllScope
+New-Alias -Name gt -Value Get-GitTree -Force -Option AllScope
 
 function Get-GitStatus { & git status -sb $args }
-New-Alias -Name s -Value Get-GitStatus -Force -Option AllScope
+New-Alias -Name gs -Value Get-GitStatus -Force -Option AllScope
 
 function Get-GitCommit { & git commit -ev $args }
-New-Alias -Name c -Value Get-GitCommit -Force -Option AllScope
+New-Alias -Name gc -Value Get-GitCommit -Force -Option AllScope
 
 function Get-GitFetch { & git fetch $args }
-New-Alias -Name f -Value Get-GitFetch -Force -Option AllScope
+New-Alias -Name gf -Value Get-GitFetch -Force -Option AllScope
 
 function Get-GitBranch { & git branch $args }
-New-Alias -Name b -Value Get-GitBranch -Force -Option AllScope
+New-Alias -Name gb -Value Get-GitBranch -Force -Option AllScope
 
 function Get-GitRemote { & git remote -v $args }
-New-Alias -Name r -Value Get-GitRemote -Force -Option AllScope
+New-Alias -Name gr -Value Get-GitRemote -Force -Option AllScope
 
 function Get-GitAdd { & git add --all $args }
 New-Alias -Name ga -Value Get-GitAdd -Force -Option AllScope
@@ -30,7 +30,7 @@ function Get-GitPull { & git pull $args }
 New-Alias -Name gpl -Value Get-GitPull -Force -Option AllScope
 
 function Get-GitCheckout { & git checkout $args }
-New-Alias -Name co -Value Get-GitCheckout -Force -Option AllScope
+New-Alias -Name gco -Value Get-GitCheckout -Force -Option AllScope
 
 function Run-CleanInstall { & mvn clean install -DskipTests }
 New-Alias -Name mvnci -Value Run-CleanInstall -Force -Option AllScope
