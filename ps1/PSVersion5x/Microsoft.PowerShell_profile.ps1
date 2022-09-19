bc875c1240993e35de3cf600b6378557aea2536e
Iuri Matos <74837498+iuriimattos2@users.noreply.github.com>
2022-09-19 01:34:22 -0300

    Update Microsoft.PowerShell_profile.ps1

--- ps1/PSVersion5x/Microsoft.PowerShell_profile.ps1
+++ ps1/PSVersion5x/Microsoft.PowerShell_profile.ps1
@@ -8,13 +8,9 @@
 #SerializationVersion           1.1.0.1
 
 # https://stackoverflow.com/a/52651577
-Set-PSReadLineOption -Colors @{Operator = "Red"; Parameter = "Red"; Command = "Red";String = "Red"}
+Set-PSReadLineOption -Colors @{Operator = "Red"; Parameter = "Red"; Command = "Red"; String = "Red" }
 
-Import-Module posh-git
-Import-Module oh-my-posh
-Set-PoshPrompt -Theme  ~/mypwsh10k.omp.json
-
-function Get-GitLog { & git log --graph --oneline --decorate $args }
+function Get-GitLog { & git log }
 New-Alias -Name gl -Value Get-GitLog -Force -Option AllScope
 
 function Get-GitTree { & git log --graph --oneline --decorate $args }
@@ -23,20 +19,18 @@ New-Alias -Name gt -Value Get-GitTree -Force -Option AllScope
 function Get-GitStatus { & git status -sb $args }
 New-Alias -Name gs -Value Get-GitStatus -Force -Option AllScope
 
+function Get-GitAdd { & git add --all }
+New-Alias -Name ga -Value Get-GitAdd -Force -Option AllScope
+
 function Get-GitCommit { & git commit -ev $args }
 New-Alias -Name gc -Value Get-GitCommit -Force -Option AllScope
 
-function Get-GitAdd { & git add $args }
-New-Alias -Name ga -Value Get-GitAdd -Force -Option AllScope
-
-function Get-AddAll { & git add . }
-New-Alias -Name gaa -Value Get-AddAll -Force -Option AllScope
-
-function Get-GitCommitWip { & Get-AddAll AND git commit -m wip }
-New-Alias -Name gwip -Value Get-GitCommit -Force -Option AllScope
-
-function Get-GitCommitWipPush { & Get-GitCommitWip AND git push }
-New-Alias -Name gsave -Value Get-GitCommitWipPush -Force -Option AllScope
+function Get-GitCommitWip {
+    param([string]$mes = "wip")
+    ga
+    git commit -m $mes
+}
+New-Alias -Name gwip -Value Get-GitCommitWip -Force -Option AllScope
 
 function Get-GitFetch { & git fetch $args }
 New-Alias -Name gf -Value Get-GitFetch -Force -Option AllScope
@@ -50,13 +44,10 @@ New-Alias -Name gbdel -Value Get-GitBranchDelete -Force -Option AllScope
 function Get-GitRemote { & git remote -v $args }
 New-Alias -Name gr -Value Get-GitRemote -Force -Option AllScope
 
-function Get-GitAdd { & git add --all $args }
-New-Alias -Name ga -Value Get-GitAdd -Force -Option AllScope
-
-function Get-GitPush { & git push $args }
+function Get-GitPush { & git push }
 New-Alias -Name gpp -Value Get-GitPush -Force -Option AllScope
 
-function Get-GitPull { & git pull $args }
+function Get-GitPull { & git pull }
 New-Alias -Name gp -Value Get-GitPull -Force -Option AllScope
 
 function Get-GitCheckout { & git checkout $args }
@@ -71,8 +62,8 @@ New-Alias -Name grs -Value Get-GitResetSoft -Force -Option AllScope
 function Run-CleanInstall { & mvn clean install -DskipTests }
 New-Alias -Name mvnci -Value Run-CleanInstall -Force -Option AllScope
 
-# Chocolatey profile
-$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
-if (Test-Path($ChocolateyProfile)) {
-  Import-Module "$ChocolateyProfile"
+function Get-GitCommitWipPush {
+    gwip
+    gp
 }
+New-Alias -Name gsave -Value Get-GitCommitWipPush -Force -Option AllScope
