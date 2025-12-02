6e740d9977b5f3050cc147dc685324d7603688bc
Iuri Matos <iuriimattos@gmail.com>
2025-12-02 18:45:20 -0300

    Update windows-network-troubleshooting.bat

--- windows-network-troubleshooting.bat
+++ windows-network-troubleshooting.bat
@@ -1,1 +1,10 @@
+# PT
+Esse comando força o Windows a redefinir ("zerar") todas as configurações de rede, renovar seu endereço IP e limpar memórias antigas de navegação para tentar corrigir problemas de internet.
+
+```bash
 netsh winsock reset && netsh int ip reset && ipconfig /release && ipconfig /renew && ipconfig /flushdns
+```
+
+## details
+
+https://gemini.google.com/share/7e9354def95f
